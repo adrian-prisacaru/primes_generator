63600dc2395a6d68ec0c3120ea226d367e1ea500
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2015-04-02 02:43:48 +0300

    test cases

---
+++ lib/primes_generator.rb
@@ -0,0 +1,13 @@
+module PrimesGenerator
+  
+  class << self
+    def is_prime?(number)
+      
+    end
+    
+    def generate(first, last)
+      
+    end
+  end
+  
+end
---
+++ test/primes_generator_test.rb
@@ -0,0 +1,59 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'test/unit'
+require 'contest'
+require_relative '../lib/primes_generator'
+
+class PrimesGeneratorTest < Test::Unit::TestCase
+  
+  context 'is prime' do
+    
+    should 'be true for primes' do
+      assert(PrimesGenerator.is_prime?(2))
+      assert(PrimesGenerator.is_prime?(3))
+      assert(PrimesGenerator.is_prime?(7))
+      assert(PrimesGenerator.is_prime?(29))
+      assert(PrimesGenerator.is_prime?(137))
+      assert(PrimesGenerator.is_prime?(541))
+      assert(PrimesGenerator.is_prime?(1063))
+    end
+    
+    should 'not be true for composite numbers' do
+      assert(!PrimesGenerator.is_prime?(4))
+      assert(!PrimesGenerator.is_prime?(9))
+      assert(!PrimesGenerator.is_prime?(49))
+      assert(!PrimesGenerator.is_prime?(100))
+    end
+    
+    should 'not be true for 1' do
+      assert(!PrimesGenerator.is_prime?(1))
+    end
+    
+    should 'not be true for 0' do
+      assert(!PrimesGenerator.is_prime?(0))
+    end
+    
+    should 'not be true for negative numbers' do
+      assert(!PrimesGenerator.is_prime?(-3))
+      assert(!PrimesGenerator.is_prime?(-11))
+    end  
+  end
+  
+  context 'generate' do
+    
+    should 'return the correct result' do
+      assert_equal([7901, 7907, 7919], PrimesGenerator.generate(7900, 7920))
+    end
+    
+    should 'include the endpoints' do
+      assert_equal([7, 11, 13, 17, 19], PrimesGenerator.generate(7, 19))
+    end
+    
+    should 'handle inverse ranges' do
+      assert_equal(PrimesGenerator.generate(1, 10), PrimesGenerator.generate(10, 1))
+    end
+  end
+  
+  
+end
